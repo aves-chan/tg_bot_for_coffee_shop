e91d301cd806f3252fb38ad8143a757d7b302e7f
Aves Chan <aves.chan@yandex.ru>
2023-12-26 19:59:30 +0300

    перенес проект на вебхуки, измен fastapi на aiohttp

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 import asyncio
 import logging
 import psycopg2
+from aiogram.enums import ParseMode
+from aiohttp import web
+
 import config
+from fastapi import FastAPI
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 
-
+from config_gitignore import WEB_SERVER_HOST_GITIGNORE, WEB_SERVER_PORT_GITIGNORE, WEBHOOK_PATH_GITIGNORE, \
+    WEBHOOK_SECRET_GITIGNORE, BASE_WEBHOOK_URL_GITIGNORE
 from keyboard.keyboard_client import kb_start_client, kb_phone_number
 from handlers.handlers_client.handlers_client_order import client_order_router
 from state.state_client import Client_state
 
 dp = Dispatcher()
+
+app = FastAPI()
+
+WEB_SERVER_HOST = WEB_SERVER_HOST_GITIGNORE
+# Port for incoming request from reverse proxy. Should be any available port
+WEB_SERVER_PORT = WEB_SERVER_PORT_GITIGNORE
+
+# Path to webhook route, on which Telegram will send requests
+WEBHOOK_PATH = WEBHOOK_PATH_GITIGNORE
+# Secret key to validate requests from Telegram (optional)
+WEBHOOK_SECRET = WEBHOOK_SECRET_GITIGNORE
+# Base URL for webhook will be used to generate webhook URL for Telegram,
+# in this example it is used public DNS with HTTPS support
+BASE_WEBHOOK_URL = BASE_WEBHOOK_URL_GITIGNORE
+
+async def on_startup(bot: Bot) -> None:
+    # If you have a self-signed SSL certificate, then you will need to send a public
+    # certificate to Telegram
+    await bot.set_webhook(f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}", secret_token=WEBHOOK_SECRET)
 
 def chek_user(id_telegram: int):
     conn = psycopg2.connect(dbname="coffee_shop", user="vsevolod", password=config.PASSWORD_POSTGRESQL, host=config.IP_MY_SERVER, port="5432")
@@ -41,9 +66,14 @@ async def start(msg: types.Message, state: FSMContext):
 dp.include_routers(client_order_router)
 
 
-async def main():
-    bot = Bot(token=config.TOKEN)
-    await dp.start_polling(bot)
+def main() -> None:
+    dp.startup.register(on_startup)
+    bot = Bot(config.TOKEN, parse_mode=ParseMode.HTML)
+    app = web.Application()
+    webhook_requests_handler = SimpleRequestHandler(dispatcher=dp, bot=bot, secret_token=WEBHOOK_SECRET)
+    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
+    setup_application(app, dp, bot=bot)
+    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
