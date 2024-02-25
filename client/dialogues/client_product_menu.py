ed2ad0f40830a6a6def923c3cb1ed646847a59e6
Aves Chan <aves.chan@yandex.ru>
2024-02-25 22:37:03 +0300

    added ScrollingGroup in dialog: dialog_product_menu window: category_menu

--- client/dialogues/client_product_menu.py
+++ client/dialogues/client_product_menu.py
@@ -1,25 +1,39 @@
+import typing
+
 from aiogram.enums import ParseMode
 from aiogram.types import CallbackQuery
 from aiogram_dialog import Dialog, Window, DialogManager
-from aiogram_dialog.widgets.kbd import Button, Row, Group, ScrollingGroup
+from aiogram_dialog.widgets.kbd import Button, Row, Group, ScrollingGroup, Cancel
 from aiogram_dialog.widgets.text import Const, Format, Multi, Jinja
 
 from client.client_state import Client_catalog_state
 from database_queries import db_queries
 
 
-async def category_getter(**kwargs) -> dict:
-    categories = db_queries.get_categories()
-    if len(categories) == 0:
-        return {}
+def generate_category_menu_buttons() -> typing.List:
+    buttons = []
+    for i in range(100):
+        i = str(i)
+        buttons.append(Button(Const(i), id=i))
+    return buttons
 
+
+category_menu_buttons = generate_category_menu_buttons()
 
 
 dialog_product_menu = Dialog(
     Window(
         Const("<b>Выберите категорию</b>"),
+        Group(
+            ScrollingGroup(
+                *category_menu_buttons,
+                id="category_menu",
+                width=2,
+                height=6
+            ),
+            Cancel(text=Const(text="Назад"))
+        ),
         parse_mode="HTML",
-        getter=category_getter,
         state=Client_catalog_state.category_menu
 
     )
